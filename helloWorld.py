c87435b1b829634d9c1c7f16736c1e7700e287a2
Wladimir.Pedrosa <wladimir.pedrosa@gmail.com>
2017-12-28 15:34:33 -0200

    1st commit on PyCharm

---
+++ helloWorld.py
@@ -0,0 +1,25 @@
+#print("hello World")
+intVal = 5
+floatVal = 21.1
+boolVal = False
+print(boolVal)
+boolVal = True
+print(intVal)
+print(floatVal)
+print(boolVal)
+
+add = 3+2
+sub = 9 - 6
+div = 10/5
+prod = 4*7
+
+operator1 = 2
+operator2 = 2
+operator3 = 2
+
+operator1 **= 2
+operator2 //= 0.5
+operator3 %= 1
+
+print(add, sub, div, prod)
+print(operator1, operator2, operator3)

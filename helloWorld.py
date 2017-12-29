edb519950bafaf2cdc23dd02636e8ca1a76c7cfe
Wladimir.Pedrosa <wladimir.pedrosa@gmail.com>
2017-12-29 08:54:31 -0200

    1st commit on PyCharm

--- helloWorld.py
+++ helloWorld.py
@@ -1,4 +1,7 @@
-#print("hello World")
+# First Test
+print("hello World")
+
+# Some Assignments
 intVal = 5
 floatVal = 21.1
 boolVal = False
@@ -8,6 +11,10 @@ print(intVal)
 print(floatVal)
 print(boolVal)
 
+""" 
+Some 
+Math Operators
+"""
 add = 3+2
 sub = 9 - 6
 div = 10/5
@@ -23,3 +30,6 @@ operator3 %= 1
 
 print(add, sub, div, prod)
 print(operator1, operator2, operator3)
+
+ordOp1 = 7 + 6 + 9 - 4 * ((9 - 2) ** 2) / 7
+ordOp2 = (6 % 4 * (7 + (7 + 2) * 3)) ** 2
